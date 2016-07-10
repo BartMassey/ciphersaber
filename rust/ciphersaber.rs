55df685e30abd408b073e87387b254801f8f7e91
Bart Massey <bart@cs.pdx.edu>
2016-07-10 10:52:53 -0700

    added ciphersaber2 reps to rust

--- rust/ciphersaber.rs
+++ rust/ciphersaber.rs
@@ -45,6 +45,8 @@ fn main() {
     opts.optflag("e", "encrypt", "encrypt input to output");
     opts.optflag("d", "decrypt", "decrypt input to output");
     opts.optopt("k", "key", "KEY", "key on command line (dangerous)");
+    opts.optopt("r", "reps", "N",
+                "number of key-scheduling reps (default 20)");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
         Err(f) => panic!(f.to_string())
@@ -74,10 +76,14 @@ fn main() {
             }
         }
     };
+    let reps = match matches.opt_str("r") {
+        Some(rs) => rs.trim().parse().expect("bad rep count"),
+        _ => 20
+    };
     
     let mut sin = stdin();
     let mut sout = stdout();
-    let mut ivb :[u8;10] = [Default::default();10];
+    let mut ivb: [u8;10] = [Default::default();10];
     match dirn {
         Dirn::Encrypt => {
             mkiv(&mut ivb);
@@ -98,12 +104,14 @@ fn main() {
         state[k] = k as u8;
     }
     let kl = keybytes.len();
-    let mut j = 0;
-    for k in 0..256 {
-        j = (j + state[k] as usize + keybytes[k % kl] as usize) & 0xff;
-        let tmp = state[k];
-        state[k] = state[j];
-        state[j] = tmp;
+    let mut i: usize = 0;
+    for _ in 0..reps {
+        for j in 0..256 {
+            i = (i + state[j] as usize + keybytes[j % kl] as usize) & 0xff;
+            let tmp = state[j];
+            state[j] = state[i];
+            state[i] = tmp;
+        }
     }
 
     let mut i: usize = 0;
