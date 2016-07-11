e15b985721fca0712c20479f65edbed0013e3a7a
Bart Massey <bart@cs.pdx.edu>
2016-07-11 14:47:30 -0700

    added ciphersaber2 reps support to c; cleaned up c key handling

--- ciphersaber.c
+++ ciphersaber.c
@@ -48,54 +48,68 @@ int main(int argc, char **argv) {
   int encrypt = 0;
   unsigned char *ivp;
   int tmp, ch, k;
-  unsigned char i, j, n;
-
-#ifdef READ_KEY
+  unsigned char i, j, n, r;
+  int reps = 20;
   char *s;
 
   /* mode */
-  if (argc == 2 && !strcmp(argv[1], "-e")) {
-    encrypt = 1;
-  } else if (argc == 2 && !strcmp(argv[1], "-d")) {
-    encrypt = 0;
-  } else {
-    fprintf(stderr, "usage: ciphersaber [-e|-d]\n");
+  while (argc > 1) {
+    if (!strcmp(argv[1], "-e")) {
+      encrypt = 1;
+      --argc, ++argv;
+      continue;
+    }
+    if (!strcmp(argv[1], "-d")) {
+      encrypt = 0;
+      --argc, ++argv;
+      continue;
+    }
+    if (argc > 2 && !strcmp(argv[1], "-r")) {
+      reps = atoi(argv[2]);
+      if (reps <= 0) {
+        fprintf(stderr, "bad reps\n");
+        return 1;
+      }
+      argc-=2, argv+=2;
+      continue;
+    }
+    if (argc > 2 && !strcmp(argv[1], "-k")) {
+      nkey = strlen(argv[2]);
+      if (nkey >= sizeof(key) - 1) {
+        fprintf(stderr, "key too long\n");
+        return 1;
+      }
+      strcpy((char *)key, argv[2]);
+      argc-=2, argv+=2;
+      continue;
+    }
+    fprintf(stderr, "usage: ciphersaber"
+            " [-e|-d] [-r <reps>] [-k <key>]\n");
     return 1;
   }
 
-  /* key acquisition */
-  /* XXX 128-bit maximum key ala getpass(3)! */
-  s = getpass("Key: ");
-  if (!s) {
-    perror("getpass");
-    return 1;
-  }
-  (void) strcpy((char *) key, s);
-  nkey = strlen((char *) key);
-  if (encrypt) {
-    s = getpass("Again: ");
+  if (nkey == 0) {
+    /* key acquisition */
+    /* XXX 128-bit maximum key ala getpass(3)! */
+    s = getpass("Key: ");
     if (!s) {
       perror("getpass");
       return 1;
     }
-    if (strcmp(s, (char *) key)) {
-      fprintf(stderr, "Key mismatch\n");
-      return 1;
+    (void) strcpy((char *) key, s);
+    nkey = strlen((char *) key);
+    if (encrypt) {
+      s = getpass("Again: ");
+      if (!s) {
+        perror("getpass");
+        return 1;
+      }
+      if (strcmp(s, (char *) key)) {
+        fprintf(stderr, "Key mismatch\n");
+        return 1;
+      }
     }
   }
-#else
-  /* mode */
-  if (argc == 3 && !strcmp(argv[1], "-e")) {
-    encrypt = 1;
-  } else if (argc == 3 && !strcmp(argv[1], "-d")) {
-    encrypt = 0;
-  } else {
-    fprintf(stderr, "usage: ciphersaber [-e|-d] <key>\n");
-    return 1;
-  }
-  nkey = strlen(argv[2]);
-  strncpy((char *)key, argv[2], nkey);
-#endif
 
   /* iv handling */
   ivp = &key[nkey];
@@ -113,12 +127,14 @@ int main(int argc, char **argv) {
   for (k = 0; k < 256; k++)
     state[k] = k;
   j = 0;
-  for (k = 0; k < 256; k++) {
-    i = k;
-    j += state[i] + key[i % nkey];
-    tmp = state[i];
-    state[i] = state[j];
-    state[j] = tmp;
+  for (r = 0; r < reps; r++) {
+    for (k = 0; k < 256; k++) {
+      i = k;
+      j += state[i] + key[i % nkey];
+      tmp = state[i];
+      state[i] = state[j];
+      state[j] = tmp;
+    }
   }
 
   /* ciphering */
